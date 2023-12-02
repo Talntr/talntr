a7be5bca7f7112a0fb641335c0b9f88b6659ca4a
chsami-talntr <150627357+chsami-talntr@users.noreply.github.com>
2023-12-02 22:54:58 +0100

    className cleanup

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,12 +4,12 @@ import { Button } from '@nextui-org/button'
 export default function Home() {
   return (
     <div className="h-full w-full flex">
-      <div className="flex place-items-center h-full bg-blue-700 flex-grow item-center justify-center text-center">
+      <div className="flex h-full bg-blue-700 flex-grow justify-center place-items-center">
         <h1>
           <Button>CONNECTEER NU</Button>
         </h1>
       </div>
-      <div className="flex place-items-center h-full bg-amber-700 flex-grow item-center justify-center text-center">
+      <div className="flex h-full bg-amber-700 flex-grow justify-center place-items-center">
         <h1>Lorem ipsum dolor sit amet.</h1>
       </div>
     </div>
