4e48a96453aa5a3336cd052be01719709bb7c272
chsami <sami.chkhachkhi@gmail.com>
2024-02-14 16:11:58 +0100

    test commit

--- frontend/src/features/home/components/banner.tsx
+++ frontend/src/features/home/components/banner.tsx
@@ -4,7 +4,7 @@ import useScreenRef from '@/src/features/home/hooks/useScreenRef'
 
 export default function Banner() {
   const [bannerText] = useScreenRef()
-
+  ///test
   return (
     <div ref={bannerText} className=" duration-1000 ease-in-out opacity-0 mb-5">
       <span className="text-3xl">Talntr - Where Skills Meet Opportunity</span>
